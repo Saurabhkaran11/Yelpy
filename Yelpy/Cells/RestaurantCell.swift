f87b027e2a2a7922e7f7b0489cdb970d04a9927d
Saurabh <Saurabhkaran112gmail.com>
2020-09-03 16:30:34 -0700

    first commit yelpy

---
+++ Yelpy/Cells/RestaurantCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantCell.swift
+//  Yelpy
+//
+//  Created by Saurabh Agrawal on 8/28/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var RestaurantCell: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
